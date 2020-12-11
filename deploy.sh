ff17f90fc50ca9d8da3308bca8aaafad0134f14a
coalyer <liyang_qifengle@126.com>
2020-12-11 18:40:54 +0800

    测试github自动部署

--- deploy.sh
+++ deploy.sh
@@ -9,34 +9,30 @@ cd docs/.vuepress/dist
 
 # deploy to github
 # echo 'b.xugaoyi.com' > CNAME
-# if [ -z "$GITHUB_TOKEN" ]; then
-#   msg='deploy'
-#   githubUrl=git@github.com:coalyer/blog.git
-# else
-#   msg='来自github actions的自动部署'
-#   githubUrl=https://${GITHUB_TOKEN}@github.com/coalyer/blog.git
-#   git config --global user.name "coalyer"
-#   git config --global user.email "liyang_qifengle@126.com"
-# fi
-# git init
-# git add -A
-# git commit -m "${msg}"
-# git push -f $githubUrl master:gh-pages # 推送到github
-
-# deploy to coding
-# echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
-if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
-  codingUrl=git@e.coding.net:coalyer/blog/blog.git
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:coalyer/blog.git
 else
   msg='来自github actions的自动部署'
-  codingUrl=https://MVOiLIZOlX:${CODING_TOKEN}@e.coding.net/coalyer/blog/blog.git
+  githubUrl=https://${GITHUB_TOKEN}@github.com/coalyer/blog.git
   git config --global user.name "coalyer"
   git config --global user.email "liyang_qifengle@126.com"
 fi
 git init
 git add -A
 git commit -m "${msg}"
-git push -f $codingUrl master # 推送到coding
+git push -f $githubUrl master:gh-pages # 推送到github
+
+# deploy to coding
+# echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
+# if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true；$CODING_TOKEN来自于github仓库`Settings/Secrets`设置的私密环境变量
+#   codingUrl=git@e.coding.net:coalyer/blog/blog.git
+# else
+#   codingUrl=https://MVOiLIZOlX:${CODING_TOKEN}@e.coding.net/coalyer/blog/blog.git
+# fi
+# git add -A
+# git commit -m "${msg}"
+# git push -f $codingUrl master # 推送到coding
 
 cd - # 退回开始所在目录
 rm -rf docs/.vuepress/dist
